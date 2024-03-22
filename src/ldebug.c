ed6d2b41d171fc97c517c88a6a11d45edb130dc0
green <25813+artgreen@users.noreply.github.com>
2024-03-22 15:29:43 -0500

    change memory model to 1 use autosegmentation turn on lint -1, bc, why not? no change in tests

--- src/ldebug.c
+++ src/ldebug.c
@@ -31,8 +31,9 @@
 
 #ifdef LUA_USE_IIGS
 #pragma noroot
-#pragma memorymodel 0
-segment "ldebug", dynamic;
+#pragma lint -1
+#pragma memorymodel 1
+segment "AUTOSEG~~~", dynamic;
 #endif
 
 
--- src/ldump.c
+++ src/ldump.c
@@ -21,8 +21,9 @@
 
 #ifdef LUA_USE_IIGS
 #pragma noroot
-#pragma memorymodel 0
-segment "ldump";
+#pragma lint -1
+#pragma memorymodel 1
+segment "AUTOSEG~~~";
 #endif
 
 typedef struct {
